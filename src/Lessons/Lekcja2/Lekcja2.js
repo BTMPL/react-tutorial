998290330904bb7f2cf8b325c0154977bbbbbdca
Bartosz Szczecinski <btm@anfo.pl>
2017-12-01 21:34:40 +0100

    Added modules writeup

--- src/Lessons/Lekcja2/Lekcja2.js
+++ src/Lessons/Lekcja2/Lekcja2.js
@@ -611,7 +611,172 @@ export default class Lesson extends React.Component {
         <Navigate prev={this.getPrev(this.props.section)} next={this.getNext(this.props.section)} />
       </div> 
     );
-  }   
+  }  
+  
+  renderModules = () => {
+    return (
+      <div>
+        <Row>
+          <Column>
+            <h2>Szybki kurs ES6 i ESNext</h2>
+            <h3>Moduły</h3>          
+            <p>
+              Jednym z problemów występującym w JavaScript od samego początku był problem braku przestrzeni nazw - każda utworzona przez nas zmienna czy funkcja, o ile nie 
+              osadzona w wewnątrz przestrzeni innej funkcji znajdowała się w przestrzeni globalnej i była dostępna jako <code>window.nazwaZmiennej</code> - w przypadku kiedy
+              korzystaliśmy z wielu bibliotek mogły pojawić się problemy z nadpisywaniem się wzajemnie funkcji czy zmiennych o tych samych nazwach.
+            </p>
+            <p>
+              Innym z problemów było samo udostępnianie i wykorzystywanie kodu - w tym celu po prostu dodawaliśmy plik z interesującym nas fragmentem JS do dokumentu HTML
+              i w momencie załadowania kod był dostępny dla całej strony. Problemy oczywiście pojawiały się w momencie zwiększenia ilości kodu - długie ładowanie, skomplikowane
+              zależności, problemy z utrzymaniem kodu rozproszonego w chaotyczny sposób po wielu plikach.
+            </p>
+            <p>
+              Problem ten próbowano rozwiązać na wiele sposobów, najpopularniejsze z nich to CommonJS i ES6 modules.
+            </p>
+            <h3>CommonJS</h3>
+          </Column>
+        </Row>
+        <Row>
+          <Column width={6}>
+            <p>
+              System modułów składa się z 2 części: pliku eksportującego dane.
+            </p>
+            <p>
+              Plik, ktróy eksportuje dane w formacie CommonJS używa notacji <code>module.exports</code> i określa dwa typy eksportów:
+            </p>
+            <ul>
+              <li>eksport domyślny - oznaczony jako <code>module.exports.default</code> - każdy moduł może mieć maksymalnie jeden eksport domyślny</li>
+              <li>eksport nazwany - oznaczony jako <code>modules.exports.NAZWA_EKSPORTU</code> - każdy moduł może deklarować dowolną ilość nazwanych eskportów</li>
+            </ul>
+            <p>
+              Wszelkie wykesportowane dane mogą zostać zaimportowane przez dowolny inny moduł/aplikację, zaś dane, które nie są eksportowane są stosowane jako prywatne API
+              modułu i jeżeli nie udostępnimy interfejsu do ich modyfikacji nie będą one mogły być w żaden sposób modyfikowane.
+            </p>            
+          </Column>
+          <Column width={6}>
+            <Example>{`
+              // Math.js
+
+              const PI = 3.14;
+              const obwodKola = (r) => 2 * PI * r;
+
+              module.exports.pi = PI;
+              module.exports.detauls = obwodKola; 
+              
+              // alternatywnie
+              module.exports = {
+                PI,
+                default: obwodKola
+              }
+            `}</Example>
+          </Column>
+        </Row>
+        <Row>
+          <Column width={6}>
+            <p>
+              W celu uzyskania dostępu do wyeksportowanych danych z innego modułu używamy funkcji <code>require</code>. W przypadku modułów CommonJS odwołania do konkretnych
+              eksportów jest dosyć oczywiste - wszystkie one stanowią pola na zaimportowanym obiekcie.
+            </p>
+            <p>
+              Domyślnie, jeżeli ścieżka do importowanego pliku nie zaczyna się od <code>.</code> oznacza to, że moduł powinien zostać odszukany w folderze <code>node_modules</code>.
+              Zachowanie to może zostać zmodyfikowane w konfiguracji webpacka.              
+            </p>
+          </Column>
+          <Column width={6}>
+            <Example>{`
+              const MyMath = require('./Math.js');
+              console.log(MyMath.PI); // 3.14;
+
+              console.log(MyMath.default(2)); // 2 * PI * r = 12.56
+
+              // możemy także zaimportować określone elementy:
+              const PI = require('./Math.js').PI;
+              const obwodKola = require('./Math.js').default;
+
+              // zaimportuj moduł NPM left-pad z katalogu node_modules
+              const leftPad = require('left-pad');
+            `}</Example>
+          </Column>
+        </Row>  
+        <Row>
+          <Column>
+            <Uwaga>
+              <h4>Uwaga</h4>
+              <p>
+                Node (a także webpack) w celu optymalizacji wydajności traktuje każdy moduł jako singleton. Oznacza to, że przy kolejnych importach modułu o dokładnie tej
+                samej ścieżce jego kod nie zostanie ponownie wywołany. Dodatkowo wszelkie wewnętrzne modyfikacje poprzez upublicznione API są dostępne również w 
+                zaimportowanych kopiach.
+              </p>         
+            </Uwaga>          
+            <h3>ES6 modules</h3>
+          </Column>
+        </Row>      
+        <Row>
+          <Column width={6}>
+            <p>
+              ES6 definiuje własny format modułów, zachowujący ideę CommonJS ale wprowadzający nową składnię. ES6 modules oferują także dodatkowe zalety w postaci tree 
+              shaking (nie importowanie kodu, który nie jest wykorzystywany w aplikacji).
+            </p>  
+            <p>
+              Odpowiednik kodu z poprzednich listingów stosujacy zapis <code>export</code>:
+            </p>          
+          </Column>
+          <Column width={6}>
+            <Example>{`
+              // Math.js
+
+              export const PI = 3.14;
+              export default obwodKola = (r) => 2 * PI * r; // zwróć uwagę na brak const
+
+              // alternatywnie
+              exports = {
+                PI
+              };
+
+              const obwodKola = (r) => 2 * PI * r; // zwróć uwagę na brak "const"
+              export default obwodKola;              
+            `}</Example>
+          </Column>
+        </Row>
+        <Row>
+          <Column width={6}>
+            <p>
+              Odpowiednio, zamiast <code>require</code> używamy konstruktu <code>import</code>. Należy zwrócić uwagę, że eksport domyślny możemy zaimportować do naszej 
+              aplikacji nadając mu dowolną nazwę - nie musi w żaden sposób pokrywać się z nazwą eksportowanego obiektu znajdującą się w <code>Math.js</code>.
+            </p>
+            <Uwaga>
+              <h4>Uwaga</h4>
+              <p>
+                Pomimo że importowanie nazwanych eksportów wygląda podobnie do destrukturyzowania nie jest nim - nie można w ten sposób wydobyć pól składowych eksportu
+                domyślnego.
+              </p>         
+            </Uwaga>             
+          </Column>
+          <Column width={6}>
+            <Example>{`
+              import { PI } from "./Math.js";
+              console.log(PI); // 3.14;
+
+              import obwod from "./Math.js";
+              console.log(obwod(2)); // 2 * PI * r = 12.56
+
+              // możemy także zaimportować wiele rzeczy razem
+              import obwod, { PI } from "./Math.js";
+
+              // lub zaimportować wszystkie nazwane eskporty:
+              import * as MyMath from "./Math.js";
+              console.log(MyMath.PI);
+
+              // możemy także zmieniać nazwy eksportów nazwanych w momencie importu:
+              import { PI as prawiePi } from "./Math.js";
+              console.log(prawiePi); // 3.14
+            `}</Example>
+          </Column>
+        </Row>                
+        <Navigate prev={this.getPrev(this.props.section)} next={this.getNext(this.props.section)} />
+      </div>
+    )
+  }
 
   renderDekoratory = () => {
     return (
